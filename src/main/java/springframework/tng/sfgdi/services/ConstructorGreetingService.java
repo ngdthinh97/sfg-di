a26174660b3d74a9c0457e3410a0446629955897
ngdthinh97 <ngdthinh.97@gmail.com>
2020-07-08 15:12:27 +0700

    Property Setter Constructor - Services

---
+++ src/main/java/springframework/tng/sfgdi/services/ConstructorGreetingService.java
@@ -0,0 +1,11 @@
+package springframework.tng.sfgdi.services;
+
+import org.springframework.stereotype.Service;
+
+@Service // Anotation @Service which tells spring that is a spring managed component to bring it into the spring context
+public class ConstructorGreetingService implements GreetingService{
+	@Override
+	public String sayGreeting() {
+		return "hello from manual - Constructor";
+	}
+}
